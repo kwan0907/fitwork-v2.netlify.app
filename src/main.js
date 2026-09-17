9a99c78fc551bf3d11fa1ee8e2b719036c19c187
kwan0907 <63507197+kwan0907@users.noreply.github.com>
2026-09-17 13:32:30 +0800

    Load operations mobile polish

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import './style.css'
 import './app-polish-round2.css'
 import './styles/clients-mobile.css'
+import './styles/operations-mobile.css'
 
 const app = createApp(App)
 const pinia = createPinia()
