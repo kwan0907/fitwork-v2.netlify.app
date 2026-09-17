c1fbff5ad98f5f0ac63eed2a2c25461a7fc90634
kwan0907 <63507197+kwan0907@users.noreply.github.com>
2026-09-17 15:55:31 +0800

    Load initial data skeleton styles

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import './styles/remaining-pages-mobile.css'
 import './styles/quick-actions.css'
 import './styles/retail-checkout-fix.css'
 import './styles/mobile-touch-safety.css'
+import './styles/initial-loading.css'
 import { useMainStore } from './stores/mainStore'
 import { installQuickActions } from './quickActions'
 
